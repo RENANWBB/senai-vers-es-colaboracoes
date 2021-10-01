65deb1adce66bd8fda87566e4394921790df9548
RENANWBB <90076175+RENANWBB@users.noreply.github.com>
2021-09-30 22:21:23 -0300

    Add files via upload

---
+++ imparepar.js
@@ -0,0 +1,10 @@
+const alunos = 10;
+
+for (var index = 0; index <= 10; index++){
+
+    if (index % 2 == 0){
+        console.log(+index+ ' Par');
+    }else if (index % 2 != 0){
+        console.log(+index+ ' Impar');
+    };
+};
